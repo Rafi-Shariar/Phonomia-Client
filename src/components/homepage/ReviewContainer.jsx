92800202be38ae1f93e8c169166c86ef9cae3d1d
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-01-13 12:25:25 +0600

    review card fixed

--- src/components/homepage/ReviewContainer.jsx
+++ src/components/homepage/ReviewContainer.jsx
@@ -5,21 +5,73 @@ const ReviewContainer = () => {
     <div className="mt-10">
       <h1
         className="text-4xl font-semibold 
-            bg-linear-to-r from-orange-600 to-yellow-300 bg-clip-text text-transparent
-            "
+        bg-linear-to-r from-orange-600 to-yellow-300 
+        bg-clip-text text-transparent"
       >
         Reviews
       </h1>
 
-      <div className="grid grid-cols-12 grid-rows-7 gap-4 h-[270px] mt-6">
-        <div className="bg-amber-100 col-span-2 row-span-4">1</div>
+      <div
+        className="
+          mt-6 gap-4
+          grid grid-cols-1
+          lg:grid-cols-12 lg:grid-rows-7
+        "
+      >
+        {/* Card 1 */}
+        <div
+          className="
+            bg-amber-100 rounded-lg
+            min-h-[120px]
+            lg:col-span-2 lg:row-span-4
+          "
+        >
+          1
+        </div>
 
-        <div className="bg-red-300 col-span-3 row-span-4">2</div>
+        {/* Card 2 */}
+        <div
+          className="
+            bg-red-300 rounded-lg
+            min-h-[120px]
+            lg:col-span-3 lg:row-span-4
+          "
+        >
+          2
+        </div>
 
-        <div className="bg-amber-500 col-span-5 row-span-3 row-start-5">3</div>
+        {/* Card 3 */}
+        <div
+          className="
+            bg-amber-500 rounded-lg
+            min-h-[120px]
+            lg:col-span-5 lg:row-span-3 lg:row-start-5
+          "
+        >
+          3
+        </div>
 
-        <div className="bg-pink-300 col-span-3 row-span-7">4</div>
-        <div className="bg-green-300 col-span-4 row-span-7">5</div>
+        {/* Card 4 */}
+        <div
+          className="
+            bg-pink-300 rounded-lg
+            min-h-[120px]
+            lg:col-span-3 lg:row-span-7
+          "
+        >
+          4
+        </div>
+
+        {/* Card 5 */}
+        <div
+          className="
+            bg-green-300 rounded-lg
+            min-h-[120px]
+            lg:col-span-4 lg:row-span-7
+          "
+        >
+          5
+        </div>
       </div>
     </div>
   );
